745170db6c48cbce10a65f9aa5da9aa0d53d9c4e
i-f-r-a-h <ifrahvermeer2@gmail.com>
2023-02-20 17:11:48 +0000

    stash changes

--- crisis-connect/client/src/routes/contact/contact.jsx
+++ crisis-connect/client/src/routes/contact/contact.jsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
+
+
 const Contact = () => {
-    return (
-        // add code here
-        <section className="contactPage">
-              <div>Contact page</div>
-        </section>
-       
+    return(
+        <main>
+            <h1>We want to hear from you.</h1>
+            <p>We are available for your comments, requests and assistance.
+                Please use the form below to contact us.
+            </p>
+        </main>
+
     )
-
 }
-
 export default Contact;
