a6546e77b096973592a4a2cca902fc4934be0508
i-f-r-a-h <ifrahvermeer2@gmail.com>
2023-02-28 16:05:40 +0000

    feat: add responsiveness to header

--- crisis-connect/client/src/routes/navigation/navigation.component.jsx
+++ crisis-connect/client/src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,6 @@
-
-import { useContext } from 'react';
-import { useState, useEffect } from 'react';
-import { Fragment } from 'react';
-import {Outlet, Link} from 'react-router-dom'
-import SiteLogo from '../../assets/logo.svg'
-import AuthModal from '../authmodal/AuthModal';
+import { useState, useEffect, useContext, Fragment } from 'react';
+import {Outlet, Link} from 'react-router-dom';
+import SiteLogo from '../../assets/logo.svg';
 import { UserContext } from '../authmodal/UserContext';
 
 
@@ -12,6 +8,10 @@ import { UserContext } from '../authmodal/UserContext';
 
 const Navigation = () => {
     const {setUserInfo, userInfo} = useContext(UserContext)
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [showNavbar, setShowNavbar] = useState(false)
+
+    // dynamic user
    
     useEffect(() => {
         fetch('http://localhost:4000/profile', {
@@ -32,59 +32,97 @@ const Navigation = () => {
         setUserInfo(null)
     }
 
-    const username = userInfo?.username
+
+    const username = userInfo?.username;
 
 
 
+      useEffect(() => {
+        const mediaQuery = window.matchMedia("(min-width: 600px)");
+        const handleMediaQueryChange = (mq) => {
+        
+            if (mediaQuery.matches) {
+                setIsSmallScreen(true);
+              } else {
+                setIsSmallScreen(false);
+              }
+        };
+        
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+        handleMediaQueryChange(mediaQuery);
+
+        return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    });
+    
+
+    
+    const handleShowNavbar = () => {
+            setShowNavbar(!showNavbar)
+    }
+
+    
 
  return(
     
      <Fragment>
-       <header>
-
-            {/* logo */}
-             <Link className='logo' to={'/'}>
-                 <img src={SiteLogo} alt="logo" className='site_logo' />
-                <p>crisis connect</p>
-            </Link>
-               
-
-
-            {/* navbar */}
-            <nav className='nav'>
-                <Link className='interactiveMap' to={'/InteractiveMap'}>
-                    Immersive Map
-                </Link>
-                <Link className='community' to={'/Community'}>
-                    Community Hub
-                </Link>
-                <Link className='contact' to={'/Contact'}>
-                    Contact
-                </Link>
-            </nav>
-
-            {/* buttons */}
-            <div className="account">
-                {username && (
-                    <>
-                        <Link to="/create">Create new post</Link>
-                        <a onClick={logout}>Logout</a>
-                    </>
-                )}
-                {!username && (
-                    <>
-                        <Link className='login' to={'/Login'}>
-                            Login
-                        </Link>
-                        <Link className='register' to={'/Register'}>
-                            Sign Up
-                        </Link>
-                    </>
-                )}
+       <header className={`header  ${showNavbar &&  'active'}`}  >
+       <div className={`header__wrapper  ${showNavbar && !isSmallScreen && 'active'}`}>
            
-            </div>
 
+                {/* logo */}
+                <Link className='logo' to={'/'}>
+                    <img src={SiteLogo} alt="logo" className='site_logo' />
+                    <p>crisis connect</p>
+                </Link>
+                
+                <div className={`header__main   ${showNavbar && !isSmallScreen && 'active header__main-mobile'}`}>
+       
+
+                {/* navbar */}
+                <nav className={`nav  ${showNavbar && !isSmallScreen && 'active'}`}>
+                    <Link className='interactiveMap' to={'/InteractiveMap'} onClick={showNavbar}>
+                        Immersive Map
+                    </Link>
+                    <Link className='community' to={'/Community'} onClick={showNavbar}>
+                        Community Hub
+                    </Link>
+                    <Link className='contact' to={'/Contact'} onClick={showNavbar}>
+                        Contact
+                    </Link>
+                </nav>
+
+                {/* buttons */}
+                <div className={`account  ${showNavbar && !isSmallScreen && 'active'}`}>
+                   
+                    {username && (
+                        <>
+                             <button className='login' onClick={logout}>Logout</button>
+                            <Link to="/create" className='register'>Create new post</Link>
+                            
+                        </>
+                    )}
+                    {!username && (
+                        <>
+                            <Link className='login' to={'/Login'} onClick={showNavbar}>
+                                Login
+                            </Link>
+                            <Link className='register' to={'/Register'} onClick={showNavbar}>
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
+            
+                </div>     
+                </div>       
+      
+            </div>
+            <div className="menu-icon" onClick={handleShowNavbar}>
+                🍔
+        </div>
+
+            
         </header>
+     
         {/* <AuthModal /> */}
         <Outlet />
       </Fragment>
