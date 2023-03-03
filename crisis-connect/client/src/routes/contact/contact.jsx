0b4cddf6bd7a806a9769900fc635f0dc45442a2c
i-f-r-a-h <ifrahvermeer2@gmail.com>
2023-03-03 09:55:49 +0000

    fix merge conflict

--- crisis-connect/client/src/routes/contact/contact.jsx
+++ crisis-connect/client/src/routes/contact/contact.jsx
@@ -1,29 +1,18 @@
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import Footer from "../footer/footer.component";
 import { Fragment } from "react";
 
 const Contact = () => {
     return(
         <Fragment>
-=======
-
-
-const Contact = () => {
-    return(
->>>>>>> development
         <main>
             <h1>We want to hear from you.</h1>
             <p>We are available for your comments, requests and assistance.
                 Please use the form below to contact us.
             </p>
         </main>
-<<<<<<< HEAD
  <Footer />
  </Fragment>
-=======
-
->>>>>>> development
     )
 }
 export default Contact;
