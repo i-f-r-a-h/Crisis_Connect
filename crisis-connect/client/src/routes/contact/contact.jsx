67affb80111f4ec0e7ea444ad6f9cc2a3c08157d
i-f-r-a-h <ifrahvermeer2@gmail.com>
2023-03-03 10:30:04 +0000

    contact page

--- crisis-connect/client/src/routes/contact/contact.jsx
+++ crisis-connect/client/src/routes/contact/contact.jsx
@@ -35,8 +35,7 @@ const Contact = () => {
 
             <button type="submit" className="button__container">Send Message</button>
         </div>
-        </Fragment>
-
+       
         <main>
             <h1>We want to hear from you.</h1>
             <p>We are available for your comments, requests and assistance.
