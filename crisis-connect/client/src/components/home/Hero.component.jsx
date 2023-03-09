53f8f46e91a76e666d10c3bbab2e12e3b2880175
i-f-r-a-h <ifrahvermeer2@gmail.com>
2023-03-09 20:14:36 +0000

    feat: add testimonials section

--- crisis-connect/client/src/components/home/Hero.component.jsx
+++ crisis-connect/client/src/components/home/Hero.component.jsx
@@ -2,11 +2,14 @@ import { Fragment } from "react";
 import PublicIcon from '@mui/icons-material/Public';
 import Diversity1Icon from '@mui/icons-material/Diversity1';
 import PeopleIcon from '@mui/icons-material/People';
+import World from "./World";
+import Experience from "./World";
 
 const Hero = () => {
     return(
         <Fragment>
-        <section className="hero">
+            <section className="hero">
+                  <Experience />
             <div className="hero__container">
                    <h1>Be the Change with Crisis Connect</h1>
                    <p>Humanitarian crises are affecting millions of people worldwide. It's time for us to come together and raise awareness about the pressing issues happening around the world.</p>
@@ -14,7 +17,8 @@ const Hero = () => {
                     <button className='explore' to={'/InteractiveMap'}>Start Exploring</button>
                      <button className='community' to={'/Community'}> <span><PeopleIcon /></span> Join the community</button>
                    </div>
-            </div>
+                </div>
+              
         </section>
         <section className="hero__banner">
             <div className="hero__banner__container">
@@ -25,7 +29,8 @@ const Hero = () => {
                 <p className="hero__banner__headings"><span ><Diversity1Icon className="hero__banner__headings__icon" /></span> Disaster Relief</p>
                 <p className="hero__banner__text"> Our community is dedicated to taking action that drives real change. With Crisis Connect, we can help create a brighter future for all across the globe.</p>
             </div>
-        </section>
+            </section>
+          
         </Fragment>
     )
 }
