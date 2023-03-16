0f32c2424b9b4cf34310e2de91a5b26f58ff12c6
i-f-r-a-h <ifrahvermeer2@gmail.com>
2023-03-16 18:47:57 +0000

    feat: add create functionality

--- crisis-connect/client/src/routes/community/createPost.jsx
+++ crisis-connect/client/src/routes/community/createPost.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
-// import {Navigate} from "react-router-dom";
-// import Editor from './Editor';
+import {Navigate} from "react-router-dom";
+import Editor from './Editor';
 
 const CreatePost = () => {
     const [title,setTitle] = useState('');
@@ -10,15 +10,37 @@ const CreatePost = () => {
     const [content,setContent] = useState('');
     const [files, setFiles] = useState('');
     const [redirect, setRedirect] = useState(false);
+    async function createNewPost(ev) {
+      const data = new FormData();
+      data.set('title', title);
+      data.set('summary', summary);
+      data.set('content', content);
+      data.set('file', files[0]);
+      ev.preventDefault();
+      const response = await fetch('http://localhost:4000/post', {
+        method: 'POST',
+        body: data,
+        credentials: 'include',
+      });
+      if (response.ok) {
+        setRedirect(true);
+      }
+      console.log(files);
+    }
 
     return (
-      <form className='CreatePost' >
+      <form onSubmit={createNewPost} className='CreatePost' >
         <input type="title"
-               placeholder={'Title'}/>
+              placeholder={'Title'}
+              value={title}
+              onChange={ev => setTitle(ev.target.value)} />
         <input type="summary"
-               placeholder={'Summary'}/>
-        <input type="file"/>
-       <ReactQuill value={content}  />
+              placeholder={'Summary'}
+              value={summary}
+              onChange={ev => setSummary(ev.target.value)} />
+        <input type="file" 
+              onChange={ev => setFiles(ev.target.files)} />
+        <Editor value={content} onChange={setContent} />
         <button style={{marginTop:'5px'}}>Create post</button>
       </form>
     );
--- crisis-connect/server/models/Post.js
+++ crisis-connect/server/models/Post.js
@@ -1,18 +1,16 @@
+import mongoose from "mongoose";
 
-
-const mongoose = require('mongoose');
-const {Schema,model} = mongoose;
+const {Schema} = mongoose;
 
 const PostSchema = new Schema({
   title:String,
   summary:String,
   content:String,
   cover:String,
-  author:{type:Schema.Types.ObjectId, ref:'User'},
+  // author:{type:Schema.Types.ObjectId, ref:'User'},
 }, {
   timestamps: true,
 });
 
-const PostModel = model('Post', PostSchema);
-
-module.exports = PostModel;
+const Post = mongoose.model("Post", PostSchema);
+export default Post;
